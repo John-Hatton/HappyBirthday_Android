b20282fcd4fbc50ccaccc72b6f7f870e320a111d
John Hatton <John.Hatton@web.de>
2024-04-21 21:37:57 +0200

    Finish Happy Birthday Tutorial

--- app/src/main/java/com/example/happybirthday_android/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday_android/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.happybirthday_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,14 +35,18 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
 //                    Greeting("Android")
-                    GreetingText(
-                        message = "Happy Birthday Android",
-                        from = "From Dad",
-                        // It is a good practice to pass the modifier attribute(s) along with the
-                        // modifier from the parent composable.
-                        // So instead of adjusting the modifier from within the body of the
-                        // GreetingText function, we do it here!
-                        modifier = Modifier.padding(8.dp)
+//                    GreetingText(
+//                        message = stringResource(R.string.happy_birthday_text),
+//                        from = stringResource(R.string.signature_text),
+//                        // It is a good practice to pass the modifier attribute(s) along with the
+//                        // modifier from the parent composable.
+//                        // So instead of adjusting the modifier from within the body of the
+//                        // GreetingText function, we do it here!
+//                        modifier = Modifier.padding(8.dp)
+//                    )
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_text),
+                        from = stringResource(R.string.signature_text)
                     )
                 }
             }
@@ -70,8 +79,28 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
         )
+    }
+}
+
+@Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box (modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+
     }
 }
 
@@ -79,6 +108,11 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday John!", from = "From Dad")
+//        GreetingText(message = "Happy Birthday John!", from = "From Dad")
+        GreetingImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(id = R.string.signature_text)
+        )
+
     }
 }
